a1017aea55dc05599608ede3f302e967c9239bb7
sayantikabanik <sayantikabanik122@gmail.com>
2022-07-11 16:05:01 +0530

    cleanup

--- analytics_framework/pipeline/process.py
+++ analytics_framework/pipeline/process.py
@@ -1,24 +1,20 @@
 import intake
 import pandas as pd
-
+import pytest
 from dagster import get_dagster_logger, job, op, Output, graph
 from analytics_framework import INTAKE_LOC
 
 from dagster.utils.log import get_dagster_logger
 logger = get_dagster_logger()
 
+catalog_path = INTAKE_LOC.joinpath("catalog_entry.yml")
 
 @op
 def read_catalog_data():
-    catalog_path = INTAKE_LOC.joinpath("catalog_entry.yml")
     catalog = intake.open_catalog(catalog_path)
     df_raw = catalog.address_sample.read()
     print(df_raw.head())
     return df_raw
-
-@op
-def
-
 
 
 @job
