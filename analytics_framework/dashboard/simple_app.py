e71b9e5a426ae6ee39d80484cdfcda5074ebaea9
sayantikabanik <sayantikabanik122@gmail.com>
2022-08-24 10:48:07 +0530

    panel app updated with basic curves

--- analytics_framework/dashboard/simple_app.py
+++ analytics_framework/dashboard/simple_app.py
@@ -1,15 +1,53 @@
 import numpy as np
 import panel as pn
-import matplotlib.pyplot as plt
-import hvplot.pandas
 import holoviews as hv
-from holoviews import opts,dim
 from bokeh.models import HoverTool
+from analytics_framework.reusable_functions.intake_cf import initiate_catalog
 hv.extension('bokeh')
+
 
 class Base:
 
     def __init__(self):
-        catalog = catalog_init()
+        self.catalog = initiate_catalog()
 
-    def line_chart_twilio_stock(self):
+    def analysis_twilio_stock(self):
+        # reading catalog data
+        df_stock_data = self.catalog.twilio_stock_price.read()
+        # creating a simple table to display raw data
+        simple_table = hv.Table(df_stock_data).opts(width=700)
+
+        # line chart showing the trend of stock prices
+        hover_close = HoverTool(tooltips=[('Stock closing price ($)', '$y')])
+        hover_open = HoverTool(tooltips=[('Stock opening price ($)', '$y')])
+
+        date_col = np.array(simple_table['date'], dtype=np.datetime64)
+        open_ = hv.Curve((date_col, df_stock_data.open),
+                         'Date', 'Stock price ($)', label='open').opts(
+                          tools=[hover_open])
+        close_ = hv.Curve((date_col, df_stock_data.close),
+                          'Date', 'Stock price ($)', label='close').opts(
+                          tools=[hover_close])
+        plot_curve_open_close = (open_ * close_).opts(width=900,
+                                                      legend_position='top_left')
+
+        return pn.Column(simple_table, plot_curve_open_close)
+
+    def plot(self):
+        view = pn.template.FastListTemplate(
+            site="Twilio stock analysis",
+            main=[self.analysis_twilio_stock()]
+        )
+        return view
+
+
+if __name__ == "__main__":
+    obj = Base()
+    pn.serve(
+        obj.plot(),
+        port=5006,
+        websocket_origin=['*'],
+        autoreload=True,
+        start=True,
+        location=True,
+        )
