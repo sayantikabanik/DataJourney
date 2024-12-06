a6698081551a8823b0d067b3bcffd892564497bb
sayantikabanik <sayantikabanik122@gmail.com>
2024-12-06 16:32:35 +0700

    removed pre-process file

--- analytics_framework/reusable_functions/pre_process.py
+++
@@ -1,8 +0,0 @@
-def analyze_data(intake_catalog_entry):
-    # Load the data via intake
-    try:
-        df_input = catalog[intake_catalog_entry].read()
-        print(f"Data loaded successfully {df_input.head()}")
-    except Exception as e:
-        print(f"Error loading data: {e}")
-        return
