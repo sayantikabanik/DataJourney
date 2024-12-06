4dff00212ed2f21f472bfc8e51923fa81b03eead
sayantikabanik <sayantikabanik122@gmail.com>
2024-12-06 16:30:17 +0700

    catalog fixes and prompts updated

--- analytics_framework/ai_modeling/advance_analysis_coral_bleeching.py
+++ analytics_framework/ai_modeling/advance_analysis_coral_bleeching.py
@@ -3,15 +3,15 @@ from azure.ai.inference import ChatCompletionsClient
 from azure.ai.inference.models import UserMessage
 from azure.core.credentials import AzureKeyCredential
 
-# central dataset input via intake catalog
 import intake
 from analytics_framework import INTAKE_LOC
 from pathlib import Path
 
+
 CATALOG_LOC = Path.joinpath(INTAKE_LOC, "catalog_entry.yml")
 catalog = intake.open_catalog(CATALOG_LOC)
 df_input_coral = catalog["global_coral_bleaching"].read()
-summary_coral = df_input_coral.describe().to_string()
+print(df_input_coral)
 
 client = ChatCompletionsClient(
     endpoint="https://models.inference.ai.azure.com",
@@ -21,7 +21,7 @@ client = ChatCompletionsClient(
 response = client.complete(
     messages=[
         UserMessage(
-            content=f"Here is a dataset summary:\n{summary_coral}\nCan you provide top 03 bleached countries and damage cause?")
+            content=f"Here is a dataset:\n{df_input_coral}\nCan you provide the code to process and find the top 03 most bleached regions")
     ],
     model="Meta-Llama-3-8B-Instruct",
     temperature=0.8,
